33723212b7fd2647e976ddc342a7178034e95add
janatscher <janatscher@gmail.com>
2026-02-18 19:56:24 +0700

    bot.py modified

--- format.py
+++ format.py
@@ -1,6 +1,7 @@
+
 def format_message(dist_num, dist_list, info_list):
     head = '📌 ' + f'<b>{dist_list[dist_num - 1].upper()}</b>' + ' <b>РАЙОН</b>\n'
-    original_info = info_list[0][dist_num-1] #не забывать про [0]
+    original_info = info_list[0][dist_num-1] #обязательно [0]
     formatted_info = []
     for i in range(len(original_info)):
         if 'Дата и время начала отключения' in original_info[i]:
@@ -12,7 +13,7 @@ def format_message(dist_num, dist_list, info_list):
             if original_info[i+5] == 'Плановое': formatted_info.append('✅ Плановое\n\n')
             else: formatted_info.append('📛 Аварийное\n\n')
             formatted_info.append('🏠 <u>ОТКЛЮЧЕННЫЕ ДОМА</u>\n')
-        elif 'улица' in original_info[i] or 'проспект' in original_info[i]:
+        elif 'улица' in original_info[i] or 'проспект' in original_info[i] or 'шоссе' in original_info[i]:
             formatted_info.append('‣ ' + original_info[i] + '\n')
             if original_info[i + 1] != original_info[-1]:
                 formatted_info.append(original_info[i + 1] + '\n')
@@ -20,5 +21,5 @@ def format_message(dist_num, dist_list, info_list):
                 formatted_info.append(original_info[i + 1] + '\n')
     end = '\nБолее подробную информацию можно найти на муниципальном портале г. Новосибирска:'
     message_listed = [head] + formatted_info + [end]
+    print(''.join(message_listed))
     return ''.join(message_listed)
-
